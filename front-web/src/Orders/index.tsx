ccbc16914b7416d3cf3b8780bcf118cd77288a7c
thiagonmiziara <thiagonmiziara@gmail.com>
2021-01-07 17:29:52 -0300

    Finalizando aula 2 sds2

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import StepsHeadaer from './StepsHeadaer'
-import './styles.css'
+import React, { useState, useEffect } from "react";
+import { fetchProducts } from "../api";
+import ProductsList from "./ProductsList";
+import StepsHeadaer from "./StepsHeadaer";
+import "./styles.css";
+import { Product } from "./types";
 
 const Orders = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+   
+  useEffect(() => {
+    fetchProducts()
+      .then((response) => setProducts(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className="orders-container">
-      <StepsHeadaer/>
+      <StepsHeadaer />
+      <ProductsList products={products} />
     </div>
-  )
-}
+  );
+};
 
 export default Orders;
---
+++ front-web/src/api.ts
@@ -0,0 +1,6 @@
+import axios from "axios";
+const API_URL = "http://localhost:8080";
+
+export const fetchProducts = () => {
+  return axios(`${API_URL}/products`);
+};
