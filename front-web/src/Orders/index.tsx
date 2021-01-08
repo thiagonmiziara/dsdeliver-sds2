5a182a4678bf2f943bb9ffd075ec2763a4d820dc
thiagonmiziara <thiagonmiziara@gmail.com>
2021-01-08 19:58:04 -0300

    Finish front-web

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,27 +1,82 @@
 import React, { useState, useEffect } from "react";
-import { fetchProducts } from "../api";
-import { OrderLocationdata, Product } from "./types";
+import { toast } from "react-toastify";
+import { fetchProducts, saveOrder } from "../api";
+import { OrderLocationData, Product } from "./types";
 import OrderLocation from "./OrderLocation";
 import ProductsList from "./ProductsList";
 import StepsHeadaer from "./StepsHeadaer";
 import "./styles.css";
+import OrderSummary from "./OrderSummary";
+import Footer from "../Footer";
+import { checkIsSelected } from "./helpers";
 
 const Orders = () => {
   const [products, setProducts] = useState<Product[]>([]);
-  const [orderLocation, setOrderLocation] = useState<OrderLocationdata>();
-   
+  const [selectedProducts, setSelectedProducts] = useState<Product[]>([]);
+  const [orderLocation, setOrderLocation] = useState<OrderLocationData>();
+  const totalPrice = selectedProducts.reduce((sum, item) => {
+    return sum + item.price;
+  }, 0);
+
   useEffect(() => {
     fetchProducts()
       .then((response) => setProducts(response.data))
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        console.log(error);
+        toast.warning("Erro ao listar produtos");
+      });
   }, []);
 
+  const handleSelectProduct = (product: Product) => {
+    const isAlreadySelected = checkIsSelected(selectedProducts, product);
+
+    if (isAlreadySelected) {
+      const selected = selectedProducts.filter(
+        (item) => item.id !== product.id
+      );
+      setSelectedProducts(selected);
+    } else {
+      setSelectedProducts((previous) => [...previous, product]);
+    }
+  };
+
+  const handleSubmit = () => {
+    const productsIds = selectedProducts.map(({ id }) => ({ id }));
+    const payload = {
+      ...orderLocation!,
+      products: productsIds,
+    };
+
+    saveOrder(payload)
+      .then((response) => {
+        toast.error(`Pedido enviado com sucesso! N°${response.data.id}`);
+        setSelectedProducts([]);
+      })
+      .catch(() => {
+        toast.warning("Erro ao enviar pedido");
+      });
+  };
+
   return (
-    <div className="orders-container">
-      <StepsHeadaer />
-      <ProductsList products={products} />
-      <OrderLocation onChangeLocation={location => setOrderLocation(location)}/>
-    </div>
+    <>
+      <div className="orders-container">
+        <StepsHeadaer />
+        <ProductsList
+          products={products}
+          onSelectProduct={handleSelectProduct}
+          selectedProducts={selectedProducts}
+        />
+        <OrderLocation
+          onChangeLocation={(location) => setOrderLocation(location)}
+        />
+        <OrderSummary
+          amount={selectedProducts.length}
+          totalPrice={totalPrice}
+          onSubmit={handleSubmit}
+        />
+      </div>
+      <Footer />
+    </>
   );
 };
 
