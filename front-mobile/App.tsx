8f97bcb96bdaac2d3bccafd84f486436d20f21ae
thiagonmiziara <thiagonmiziara@gmail.com>
2021-01-09 18:31:20 -0300

    Add orders list

--- front-mobile/App.tsx
+++ front-mobile/App.tsx
@@ -3,8 +3,7 @@ import AppLoading from "expo-app-loading";
 import { StyleSheet, Text, View } from "react-native";
 import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from "@expo-google-fonts/open-sans";
 import { StatusBar } from "expo-status-bar";
-import Header from "./src/Header";
-import Home from "./src/Home";
+import Routes from "./src/Routes";
 
 const App = () => {
   let [fontsLoaded] = useFonts({
@@ -18,8 +17,7 @@ const App = () => {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Header />
-      <Home/>
+      <Routes/>
     </View>
   );
 }
--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,31 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 
 const Header = () => {
+  const navigation = useNavigation();
+
+  const handleOnPress = () => {
+    navigation.navigate("Home");
+  };
+
   return (
-    <View style={styles.container}>
-     <Image source={require('../assets/logo.png')} />
-      <Text style={styles.text}>DS Delivery</Text>
-     </View>
+    <TouchableWithoutFeedback onPress={handleOnPress}>
+      <View style={styles.container}>
+        <Image source={require("../assets/logo.png")} />
+        <Text style={styles.text}>DS Delivery</Text>
+      </View>
+    </TouchableWithoutFeedback>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#da5c5c',
+    backgroundColor: "#da5c5c",
     height: 90,
     paddingTop: 50,
-    flexDirection:'row',
-    justifyContent: 'center'
+    flexDirection: "row",
+    justifyContent: "center",
   },
-  text:{
-    fontWeight: 'bold',
+  text: {
+    fontWeight: "bold",
     fontSize: 18,
     lineHeight: 25,
     letterSpacing: -0.24,
-    color:'#fff',
+    color: "#fff",
     marginLeft: 15,
-    fontFamily: 'OpenSans_700Bold'
+    fontFamily: "OpenSans_700Bold",
   },
 });
- export default Header;
+export default Header;
